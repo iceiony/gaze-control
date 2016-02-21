d19f7fb575c87e8bed771db399c80e0548e1d153
Adrian Ionita <iceiony@Adrians-MacBook-Air.local>
2016-02-21 18:09:02 +0000

    finish rashej rao implementation according to journal paper

--- radial_basis/rashej_rao.m
+++ radial_basis/rashej_rao.m
@@ -18,31 +18,92 @@ function rashej_rao()
     mu = initialiseRandomBeliefCentres(11);
     sigma = sqrt(0.05);
     v = zeros(11,1);
+    W = zeros(11,3);
     
+    %setup start trial
     belief = [0.5 0.5];
     coherence = randi(length(prob_obs));
     state = randi(2);
-    for t=1:10   
-        value_beilef = estimateValue(belief,v,mu,sigma);
-        
-        observation = observationProbability(state,prob_obs(coherence));
-
-        belief = beliefUpdate(belief,observation);
-        disp(belief);
-    end
-    disp(state);
-    disp(prob_obs(coherence));
     
+    %when a new trial starts, do a free sample observation
+    observation = observationProbability(state,prob_obs(coherence));
+    belief = beliefUpdate(belief,observation);
+    
+    %rememeber reward for plotting
+    reward = zeros(14000,1);
+    for t=1:length(reward)   
+        %phi is g(b_t) from Rashej Rao paper 2010 paper
+        phi = estimateBeliefPoints(belief,mu,sigma); 
+        value_beilef = phi * v; 
+
+        action = selectActionToTake(phi,W);
+        
+        switch action
+            case a.sample
+                reward(t) = -1;
+                observation = observationProbability(state,prob_obs(coherence));
+                belief = beliefUpdate(belief,observation);
+            otherwise 
+                %give rewards if correct action taken
+                if action == a.left && state == s.left
+                    reward(t) = 20;
+                elseif action == a.right && state == s.right
+                    reward(t) = 20;
+                else
+                    reward(t) = -400;
+                end
+                
+                %start a new trial
+                belief = [0.5 0.5];
+                coherence = randi(length(prob_obs));
+                state = randi(2);
+                
+                %when a new trial starts, do a free sample observation
+                observation = observationProbability(state,prob_obs(coherence));
+                belief = beliefUpdate(belief,observation);
+        end
+        
+        %compute next belief estimate
+        value_new_belief = estimateBeliefPoints(belief,mu,sigma) * v;
+        
+        %update belief centers and output weights V and W 
+        td_error = reward(t) + value_new_belief - value_beilef;
+        
+        diffs = repmat(belief,size(mu,1),1) - mu;
+        mu = mu + 0.0005 * td_error * phi * v * 2 * diffs / sigma^2;
+        v = v + 0.0005 * td_error * phi';       
+        
+        W(:,action) = 2.5 * 10^-7 * td_error * phi;
+    end
+%     
+%     moving_window = ones(1,500);
+%     sum_reward_window = conv(reward,moving_window);
+%     sum_reward_window = sum_reward_window(500:end-500);
+%     plot(1:length(sum_reward_window),sum_reward_window);
+%     hold on;
+%     value = [];
+%     for t = 0:0.1:1
+%         belief = [t 1-t];
+%         phi = estimateBeliefPoints(belief,mu,sigma); 
+%         value(end+1) = phi * v; 
+%     end
+%     plot(0:0.1:1,value);
     
     
 %==========================================================================
-    function [value] = estimateValue(belief,v,mu,sigma)
-        phi = [];
+    function [action] = selectActionToTake(phi,W)
+        action_probability = exp(phi*W);
+        action_probability = action_probability ./ sum(action_probability);
+        action = find(cumsum(action_probability) >= rand(1));
+        action = action(1);
+    end
+
+    function [phi] = estimateBeliefPoints(belief,mu,sigma)
+        phi = zeros(1,size(mu,1));
         for i=1:size(mu,1)
             distance = sum((belief - mu(i,:)).^2);
             phi(i) = exp( -distance / (2*sigma^2) );
         end
-        value = phi * v; 
     end
 
     function [observation] = observationProbability(state,prob_obs)
