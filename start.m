7f7863f8a6bd736db5cc7a3d591f85ce1d4ae93d
Adrian Ionita <iceiony@yahoo.com>
2016-03-02 16:00:10 +0000

    add presentation slides

--- start.m
+++ start.m
@@ -61,7 +61,7 @@ for t=1:length(reward)
                 end
 
                 obs = plot(fix(1),fix(2),'.g','markersize',20);
-                pause(0.01);
+                input('');
             end
         otherwise % the action was to pick an object with a certain uncertainty
             target = landmarks(action);
