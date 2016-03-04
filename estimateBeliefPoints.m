8976f69566c14795125d3d1efaa449ebc193eea3
Adrian Ionita <iceiony@yahoo.com>
2016-03-04 22:27:11 +0000

    fix belief point generation and estimation

--- estimateBeliefPoints.m
+++ estimateBeliefPoints.m
@@ -3,8 +3,14 @@ function [ phi] = estimateBeliefPoints(scene,landmarks,particles,mu,sigma )
     
     for i=1:length(landmarks)
         eigenValues = eig(cov(particles(i).positions)) ./ [scene.height;scene.width;] ;
+        
+        eigenValues(1) = min(eigenValues(1),1);
+        eigenValues(2) = min(eigenValues(2),1);
+        
         beliefState(:,i) = [eigenValues' landmarks(i).value];
     end
+    
+%     disp(beliefState');
     
     phi = zeros(1,size(mu,1));
     for i=1:size(mu,1)
