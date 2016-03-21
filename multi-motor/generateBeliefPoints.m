dd0ea7f8e8e340433caf2e33d78a44852efc846a
Adrian Ionita <iceiony@yahoo.com>
2016-03-21 17:47:53 +0000

    add adjustments that make the learning cosistantly converge

--- multi-motor/generateBeliefPoints.m
+++ multi-motor/generateBeliefPoints.m
@@ -1,8 +1,12 @@
-function [mu,sigma] = generateBeliefPoints(pointsCount, dimSize )
+function [mu,sigma] = generateBeliefPoints(pointsCount, dimSize,sigma )
     %take random values to partition possible the space of possible values
     beliefSpace = rand(20^(dimSize/3) * 10, dimSize )*2 - .5;
     
-    [idx,mu,~,d] = kmeans(beliefSpace,pointsCount);
+    distributed = floor(pointsCount*2/3);
+    [idx,mu,~,d] = kmeans(beliefSpace,distributed);
     
-    sigma = ones(size(d))* 0.25;
+    mu = [mu ; rand(pointsCount-distributed,dimSize)*2 - .5];
+    
+    sigma = ones(pointsCount) * sigma;
+%     sigma = var(d).^(1/2) / 5; 
 end
--- multi-motor/train.m
+++ multi-motor/train.m
@@ -1,11 +1,11 @@
-clear all;
+clear all; 
 close all;
 
 GRASP_THRESHOLD = 5;
 LANDMARK_COUNT = 2;
 PARTICLE_COUNT = 200;
 
-%initialise scene 
+%initialise sceneç 
 rng('shuffle');
 scene = struct('width',700,'height',500);
 
@@ -13,13 +13,13 @@ landmarks = generateLandmarks(scene,LANDMARK_COUNT);
 particles = generateParticles(scene,landmarks,PARTICLE_COUNT);
 
 %grasp system weights 
-[mu,sigma] = generateBeliefPoints(25,3);
+[mu,sigma] = generateBeliefPoints(30,3,.25);
 kernel = @(x) gaussianKernel(x,mu,sigma);
 V = zeros(1 + size(mu,1),1);
 W = zeros(1 + size(mu,1),2); 
 
 %gaze system weights
-[muP,sigmaP] = generateBeliefPoints(50,3*LANDMARK_COUNT);
+[muP,sigmaP] = generateBeliefPoints(60,3*LANDMARK_COUNT,.25);
 kernelP = @(x) gaussianKernel(x,muP,sigmaP);
 VP = zeros(1+size(muP,1),1);
 WP = zeros(1+size(muP,1),2);
@@ -50,7 +50,13 @@ for t=1:length(reward)
         beliefStateNew = generateBeliefState(scene,landmarks(idx),particlesNew(idx));
         phiNew = [1 kernel(beliefStateNew)];
         
-        gazeReward = sum(phiNew * W(:,1) - phiOld * W(:,1));
+%         oldProb = exp(phiOld * W);
+%         oldProb = oldProb / sum(oldProb);
+%         
+%         newProb = exp(phiNew * W);
+%         newProb = newProb / sum(newProb);
+        
+        gazeReward = gazeReward + sum(phiNew * W(:,1) - phiOld * W(:,1));
     end
     
     gazeValue = phi * VP;
@@ -61,14 +67,15 @@ for t=1:length(reward)
     particles = particlesNew; 
     
     %--------------GRASPING-----------------
-    phiOld = zeros(2,size(V,1));
+    phiOld = zeros(LANDMARK_COUNT,size(V,1));
+    beliefStateOld = zeros(LANDMARK_COUNT,3);
     actionRewards = zeros(2,1);
     actionTaken = zeros(2,1);
     for idx = 1:LANDMARK_COUNT
         actionRewards(idx) = -1;
         
-        beliefStateOld = generateBeliefState(scene,landmarks(idx),particles(idx));
-        phiOld(idx,:) = [1 kernel(beliefStateOld)];
+        beliefStateOld(idx,:) = generateBeliefState(scene,landmarks(idx),particles(idx));
+        phiOld(idx,:) = [1 kernel(beliefStateOld(idx,:))];
         actionTaken(idx) = selectActionToTake(phiOld(idx,:),W);
         
         if actionTaken(idx) ~= 1
@@ -106,6 +113,9 @@ for t=1:length(reward)
         td_error = actionRewards(idx) + beliefValuesNew - beliefValues;
         V = V + 0.001 * phiOld(idx,:)' * td_error;   
         W(:,actionTaken(idx)) = W(:,actionTaken(idx)) + 0.0005 * phiOld(idx,:)' * td_error;
+        
+        diffs = 2 * (repmat(beliefStateOld(idx,:),size(mu,1),1) - mu) / sigma(1)^2;
+        mu = mu + 2.5 * 10^-9 * td_error * phiOld(idx,:) * V * diffs;
     end
     
     reward(t) = sum(actionRewards);
