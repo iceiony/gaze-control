89ce2fb48fc9be81cfc9b993d061b16e09182d29
Adrian Ionita <iceiony@yahoo.com>
2016-03-29 13:01:16 +0100

    sound multi_run allarm when code execution over

--- multi-motor/multi_run.m
+++ multi-motor/multi_run.m
@@ -1,19 +1,28 @@
-% 
-% reward_try = [];
-% for i = 1 : 4
-%     train;
-%     file_name = '9_learntGaze.txt';
-%     reward_try = load(file_name);
-%     reward_try(end+1,:) = sum_reward_window;
-%     save(file_name,'reward_try','-ascii');
-% end
-% % 
+try 
+    
+    reward_try = [];
+    for i = 1 : 5
+        train;
+        file_name = '13_learntGaze.txt';
+        reward_try = load(file_name);
+        reward_try(end+1,:) = sum_reward_window;
+        save(file_name,'reward_try','-ascii');
+    end
+    
 
-reward_try = [];
-for i = 1 : 4
-    trainRandom;
-    file_name = '9_randomGaze.txt';
-    reward_try = load(file_name);
-    reward_try(end+1,:) = sum_reward_window;
-    save(file_name,'reward_try','-ascii');
+    reward_try = [];
+    for i = 1 : 2
+        
+        trainRandom;
+        file_name = '13_randomGaze.txt';
+        
+        
+        reward_try = load(file_name);
+        reward_try(end+1,:) = sum_reward_window;
+        save(file_name,'reward_try','-ascii');
+    end
+catch exception
 end
+    
+load handel
+sound(y,Fs)
