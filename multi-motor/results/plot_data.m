05bbd263a6e7a803b4ac202f31f76c20f2b489f0
Adrian Ionita <iceiony@yahoo.com>
2016-03-31 21:16:43 +0100

    remove obsolete result files

--- multi-motor/results/plot_data.m
+++ multi-motor/results/plot_data.m
@@ -1,20 +1,21 @@
 figure();
 hold on;
 
-learntGazeReward = load('9_learntGaze.txt');
+learntGazeReward = load('16_learntGaze.txt');
 % plot(learntGazeReward','b');
 mean_reward = mean(learntGazeReward);
 standard_error = std(learntGazeReward)/sqrt(size(learntGazeReward,1));
-shadedErrorBar([],mean_reward,standard_error,'b',1);
+learningPlots = shadedErrorBar([],mean_reward,standard_error,'b',1);
 % plot(mean_reward,'b.');
 
 % 
-randomGazeReward = load('9_randomGaze.txt');
+randomGazeReward = load('16_randomGaze.txt');
 % plot(randomGazeReward','r');
 mean_reward = mean(randomGazeReward);
 standard_error = std(randomGazeReward)/sqrt(size(randomGazeReward,1));
-shadedErrorBar([],mean_reward,standard_error,'r',1); 
+randomPlots = shadedErrorBar([],mean_reward,standard_error,'r',1); 
 % plot(mean_reward,'r.');
 
 xlabel('time step');
 ylabel('reward');
+% legend([learningPlots.mainLine randomPlots.mainLine],'learnt gaze','random gaze','location','northwest')
