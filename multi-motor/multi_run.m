79728e4ba53fc2f409d09b076ecfee38dc45a940
Adrian Ionita <iceiony@yahoo.com>
2016-04-02 23:42:58 +0100

    uncomment multi run code

--- multi-motor/multi_run.m
+++ multi-motor/multi_run.m
@@ -1,19 +1,19 @@
 try 
 %     
-%     reward_try = [];
-%     while true
-%         train;
-%         file_name = '17_learntGaze.txt';
-%         reward_try = load(file_name);
-%         reward_try(end+1,:) = sum_reward_window;
-%         save(file_name,'reward_try','-ascii');
-%   
-%         disp(fprintf('[learning = %d]      ',size(reward_try,1))); 
-%         
-%         if size(reward_try,1) == 100 
-%             break;
-%         end
-%     end
+    reward_try = [];
+    while true
+        train;
+        file_name = '17_learntGaze.txt';
+        reward_try = load(file_name);
+        reward_try(end+1,:) = sum_reward_window;
+        save(file_name,'reward_try','-ascii');
+  
+        disp(fprintf('[learning = %d]      ',size(reward_try,1))); 
+        
+        if size(reward_try,1) == 100 
+            break;
+        end
+    end
     
 
     reward_try = [];
